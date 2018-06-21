7bf92d412243e5376316a05ff9380f3bb5faccc0
Goran93 <gg.goran93@gmail.com>
2018-06-21 16:17:24 +0200

    Added gdpr functionallities

--- constants/endpoints.js
+++ constants/endpoints.js
@@ -37,7 +37,9 @@ export const API = config => {
             return url;
         },
         ssoCookie: subdomain =>
-            `https://${subdomain}.accounts.staging-v2.inplayer.com/sso/cookie`, //NOTE: hardcoded URL
+            `https://${subdomain}.accounts.staging-v2.inplayer.com/sso/cookie`, //NOTE: hardcoded URL,
+        deleteAccount: `${config.BASE_URL}/accounts/erase`,
+        exportData: `${config.BASE_URL}/accounts/export`,
         //Asset
         checkAccess: id => `${config.BASE_URL}/items/${id}/access`,
         checkFreeTrial: id =>
