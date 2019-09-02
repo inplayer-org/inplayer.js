769655e141d940f46d7c781c645ae9f4c5d01722
Ljubomir Ignov <lignov@localhost.localdomain>
2019-09-02 15:18:32 +0200

    Added types to index.d.ts.

--- src/index.d.ts
+++ src/index.d.ts
@@ -104,7 +104,6 @@ export declare interface SocialURLs {
   google: string;
 }
 
-
 export declare interface ListSocialURLs {
   social_urls: SocialURLs[];
 }
@@ -131,7 +130,6 @@ export declare interface RestrictionSettingsData {
   created_at: number;
   updated_at: number;
 }
-
 
 export declare class Account {
   constructor(config: object);
@@ -163,9 +161,7 @@ export declare class Account {
     merchantUuid: string
   ): Promise<AxiosResponse<GetRegisterField>>;
   deleteAccount(data: DeleteAccountData): Promise<AxiosResponse<void>>;
-  exportData(
-    data: DeleteAccountData
-  ): Promise<AxiosResponse<CommonResponse>>;
+  exportData(data: DeleteAccountData): Promise<AxiosResponse<CommonResponse>>;
   sendPinCode(brandingId: number): Promise<AxiosResponse<CommonResponse>>;
   validatePinCode(pinCode: string): Promise<AxiosResponse<CommonResponse>>;
   loadMerchantRestrictionSettings(
@@ -178,30 +174,144 @@ export interface CodeAccessData {
   code: string;
 }
 
+export declare interface AccessControlType {
+  id: number;
+  name: string;
+  auth: boolean;
+}
+
+export declare interface ItemType {
+  id: number;
+  name: string;
+  content_type: string;
+  host: string;
+  description: string;
+}
+
+export declare interface ItemDetailsV1 {
+  id: number;
+  merchant_id: number;
+  merchant_uuid: string;
+  is_active: boolean;
+  title: string;
+  access_control_type: AccessControlType;
+  item_type: ItemType;
+  age_restriction: Record<string, number>;
+  metadata: Record<string, string>[];
+  created_at: number;
+  updated_at: number;
+}
+
+export declare interface Item {
+  id: number;
+  merchant_id: number;
+  merchant_uuid: string;
+  is_active: boolean;
+  title: string;
+  access_control_type: AccessControlType;
+  item_type: ItemType;
+  age_restriction: Record<string, number>;
+  metadata: Record<string, string>;
+  created_at: number;
+  update_at: number;
+}
+
+export declare interface AccessType {
+  id: number;
+  name: string;
+  quantity: number;
+  period: string;
+}
+
+export declare interface TrialPeriod {
+  quantity: number;
+  period: string;
+  description: string;
+}
+
+export declare interface SetupFee {
+  fee_amount: number;
+  description: string;
+}
+
+export declare interface GeoRestriction {
+  id: number;
+  country_iso: string;
+  country_set_id: number;
+  type: string;
+}
+
+export declare interface GetAccessFee {
+  id: number;
+  merchant_id: number;
+  amount: number;
+  currency: string;
+  description: string;
+  item: Item;
+  access_type: AccessType;
+  trial_period: TrialPeriod;
+  setup_fee: SetupFee;
+  geo_restriction: GeoRestriction;
+  expires_at: number;
+}
+
+export declare interface ExternalItemDetails extends ItemDetailsV1 {
+  access_fees: GetAccessFee[];
+  metahash: Record<string, string>;
+}
+
+export declare interface PackageDetails {
+  id: number;
+  merchant_id: number;
+  is_active: boolean;
+  title: string;
+  content: string;
+  item_type: ItemType;
+  metadata: Record<string, string>;
+  items: number;
+}
+
+export declare interface GetMerchantPackage {
+  total: number;
+  page: number;
+  offset: number;
+  limit: number;
+  collection: PackageDetails;
+}
+
+export declare interface CloudfrontUrl {
+  video_url: string;
+}
+
 export declare class Asset {
   constructor(config: object, Account: Account);
 
   checkAccessForAsset(id: number): object;
   isFreeTrialUsed(id: number): object;
-  getAsset(assetId: number, merchantUuid: string): object;
+  getAsset(
+    assetId: number,
+    merchantUuid: string
+  ): Promise<AxiosResponse<ItemDetailsV1>>;
   getExternalAsset(
     assetType: string,
     externalId: string,
     merchantUuid: string
-  ): object;
-  getPackage(id: number): object;
-  getAssetAccessFees(id: number): object;
+  ): Promise<AxiosResponse<ExternalItemDetails>>;
+  getPackage(id: number): Promise<AxiosResponse<GetMerchantPackage>>;
+  getAssetAccessFees(id: number): Promise<AxiosResponse<GetAccessFee>>;
   getAssetsHistory(
     size?: number,
     page?: number,
     startDate?: string,
     endDate?: string
-  ): object;
-  getFreemiumAsset(accessFeeId: number): object;
+  ): object[];
   requestCodeAccess(data: CodeAccessData): object;
-  getAccessCode(assetId: number): object;
+  getAccessCode(assetId: number): object | null;
   releaseAccessCode(assetId: number): object;
-  getCloudfrontURL(assetId: number, videoUrl: string): object;
+  getCloudfrontURL(
+    assetId: number,
+    videoUrl: string
+  ): Promise<AxiosResponse<CloudfrontUrl>>;
 }
 
 export declare class Branding {
