db7a6efd17fe5267ef017c90781b503c68b4ae5d
srdjanRakic <srgjan.rakic@gmail.com>
2019-10-04 12:56:39 +0200

    Removed unused util functions

--- src/Utils/index.ts
+++ src/Utils/index.ts
@@ -1,82 +1,10 @@
-/* eslint-disable */
-export async function checkStatus(response: any) {
-  if (!response.ok) {
-    const error: any = new Error(response.statusText);
-
-    error.response = await response.json();
-    throw error;
-  }
-
-  return response;
-}
-
-export const errorResponse = (statusCode = 400, body = {}) => {
-  if (statusCode < 400) {
-    statusCode = 400;
-  }
-
-  const err = new Error(String(statusCode));
-
-  throw err;
-};
-
-const buildParams = (prefix: any, obj: any, add: any) => {
-  let name; let i; let l; const rbracket = /\[\]$/;
-
-  if (obj instanceof Array) {
-    for (i = 0, l = obj.length; i < l; i++) {
-      if (rbracket.test(prefix)) {
-        add(prefix, obj[i]);
-      } else {
-        buildParams(
-          `${prefix}[${typeof obj[i] === 'object' ? i : ''}]`,
-          obj[i],
-          add,
-        );
-      }
-    }
-  } else if (typeof obj === 'object') {
-    // Serialize object item.
-    for (name in obj) {
-      buildParams(`${prefix}[${name}]`, obj[name], add);
-    }
-  } else {
-    // Serialize scalar item.
-    add(prefix, obj);
-  }
-};
-
-export const parseJSON = (response: any) => response.json();
-
-export const params = (a: any) => {
-  let prefix; const s: any = []; let name; const r20 = /%20/g; let
-    output;
-
-  const add = (key: any, value: any) => {
-    // If value is a function, invoke it and return its value
-    value = typeof value === 'function' ? value() : value === null ? '' : value;
-    s[s.length] = `${encodeURIComponent(key)}=${encodeURIComponent(value)}`;
-  };
-  if (a instanceof Array) {
-    for (name in a) {
-      add(name, a[name]);
-    }
-  } else {
-    for (prefix in a) {
-      buildParams(prefix, a[prefix], add);
-    }
-  }
-  output = s.join('&').replace(r20, '+');
-  return output;
-};
-
-export const buildURLwithQueryParams = (urlString: string, params: any) => {
+export const buildURLwithQueryParams = (urlString: string, params: string[]) => {
   const {
     url,
-    url: { searchParams }
+    url: { searchParams },
   } = { url: new URL(urlString) };
 
-  Object.keys(params).forEach(key => {
+  Object.keys(params).forEach((key: string) => {
     searchParams.set(key, params[key]);
   });
 
